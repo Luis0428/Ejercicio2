e2eefd25b47e7ea20a62e9f6ef35d52bb7f4b0a3
Luis Gerardo <bis980428@gmail.com>
2021-04-30 19:46:08 -0500

    primer commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,3 @@
-import '../styles/index.scss';
-
-console.log('webpack starterkit');
+import { User } from "./interface";
+let Usuario = new User();
+console.log(Usuario.Busqueda('ricardo.jimenez@axity.com'));
---
+++ src/scripts/index.ts
@@ -0,0 +1,6 @@
+import { idText } from "../../node_modules/typescript/lib/typescript";
+import {User} from "./interface"
+
+let Usuario = new User();
+
+console.log(Usuario.Busqueda('ricardo.jimenez@axity.com'));
